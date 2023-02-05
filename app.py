a61391322f634b69f0c580996c77f3629b3b1de2
summerorange <hyang9387@gmail.com>
2023-02-05 21:30:41 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,96 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+from flask_bootstrap import Bootstrap
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Bootstrap(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
+
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('index.html', students=students)
+
+
+@app.route('/<int:student_id>/')
+def student(student_id):
+    variable_student = Student.query.get_or_404(student_id)
+    return render_template('student.html', student=variable_student)
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+        var_student = Student(firstname=firstname,
+                          lastname=lastname,
+                          email=email,
+                          age=age,
+                          bio=bio)
+        db.session.add(var_student)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+@app.route('/<int:student_id>/edit/', methods=('GET', 'POST'))
+def edit(student_id):
+    var_student = Student.query.get_or_404(student_id)
+
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+
+        var_student.firstname = firstname
+        var_student.lastname = lastname
+        var_student.email = email
+        var_student.age = age
+        var_student.bio = bio
+
+        db.session.add(var_student)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', student=var_student)
+
+
+@app.post('/<int:student_id>/delete/')
+def delete(student_id):
+    var_student = Student.query.get_or_404(student_id)
+    db.session.delete(var_student)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+app.run(debug=True)
